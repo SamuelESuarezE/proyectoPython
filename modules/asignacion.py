d25ac921456c5c39d062854193cb097f860400a6
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-02-07 07:20:35 -0500

    fix: :bug: Corregi bugs en menu de asignacion

--- modules/asignacion.py
+++ modules/asignacion.py
@@ -37,39 +37,52 @@ def menu():
     /    \\\___ \ )(( (_ \/    //    \( (__  )((  O )/    /
     \_/\_/(____/(__)\___/\_)__)\_/\_/ \___)(__)\__/ \_)__)
     """)
-    
-    id = input("Ingrese el documento del camper que desea asignar: ")
-    
-    for camper in campersList:
-        if camper.get("ID") == id:
-            info = {
-                "CodigoRuta": mostrarRutas(),
-                "CodigoSala": input("Digite el codigo de la sala:\n\t1. Sputnik\n\t2. Apolo\n\t3. Artemis\nEscriba el codigo de la sala:"),
-                "ID-trainer": input("Digite ID del trainer: "),
-                "fechaInicio": input("Fecha inicio: "),
-                "fechaFin": input("Fecha fin: "),
-                "Horario": input("Horario:\n\t1. Mañana: 6 am a 10 am\n\t2. Mañana: 10 am a 2 pm\n\t3. Tarde: 2 pm a 6 pm\n\t4. Tarde: 6 pm a 10 pm\nEscriba el codigo del horario: "),
-            }
+    while True:
+        id = input("Ingrese el documento del camper que desea asignar: ")
+        for camper in campersList:
+            if camper.get("ID") == id:
+                if camper["Estado"] == "Aprobado":
+                    info = {
+                        "CodigoRuta": mostrarRutas(),
+                        "CodigoSala": input("Digite el codigo de la sala:\n\t1. Sputnik\n\t2. Apolo\n\t3. Artemis\n"),
+                        "ID-trainer": input("Digite ID del trainer: "),
+                        "fechaInicio": input("Fecha inicio: "),
+                        "fechaFin": input("Fecha fin: "),
+                        "Horario": input("Digite codigo de horario:\n\t1. Mañana: 6 am a 10 am\n\t2. Mañana: 10 am a 2 pm\n\t3. Tarde: 2 pm a 6 pm\n\t4. Tarde: 6 pm a 10 pm\n"),
+                    }
 
-            for ruta in rutasList:
-                if info.get("CodigoRuta") == ruta.get("Codigo"):
-                    camper["Ruta"] = ruta.get("Nombre")
-            for sala in salasList:
-                if info.get("CodigoSala") == sala.get("Codigo"):
-                    camper["Sala"] = sala.get("Nombre")
-                    sala["campers"].append(id)
-            for trainer in trainersList:
-                if info.get("ID-trainer") == trainer.get("ID"):
-                    camper["Trainer"] = trainer.get("Nombre")
-            camper["fechaInicio"]=info.get("fechaInicio")
-            camper["fechaFin"]=info.get("fechaFin")
-            camper["Horario"]=info.get("Horario")
+                    for ruta in rutasList:
+                        if info.get("CodigoRuta") == ruta.get("Codigo"):
+                            camper["Ruta"] = ruta.get("Nombre")
 
-            
+                    for sala in salasList:
+                        if camper.get("ID") in sala["campers"]:
+                            print("\n- El camper ya esta registrado en una sala de entrenamiento.")
+                            print("- Si desea cambiar la sala, debe hacer el registro del camper nuevamente.")
+                            break
+                        else:
+                            if len(sala["campers"]) < sala.get("Capacidad"):
+                                if info.get("CodigoSala") == sala.get("Codigo"):
+                                    camper["Sala"] = sala.get("Nombre")
+                                    sala["campers"].append(id)
+                            else:
+                                print("\n - La sala asignada esta llena.")
+                                break
+
+                    for trainer in trainersList:
+                        if info.get("ID-trainer") == trainer.get("ID"):
+                            camper["Trainer"] = trainer.get("Nombre")
+                    camper["fechaInicio"]=info.get("fechaInicio")
+                    camper["fechaFin"]=info.get("fechaFin")
+                    camper["Horario"]=info.get("Horario")
+                else:
+                    return print("\n- El camper no esta aprobado.")
     
-    
-    with open("modules/storage/data.json", "w") as f:
-        data = json.dumps(baseDeDatos, indent=4)
-        f.write(data)
+        with open("modules/storage/data.json", "w") as f:
+            data = json.dumps(baseDeDatos, indent=4)
+            f.write(data)
+        print("\n- Si el sistema no permite asignar al camper, es porque el camper no esta registrado.")
+        input("\nPresione enter para continuar...")
+        break
 
     
